2becea501f94fb37f72f8c111ac728b129c39b32
Pranav Kumar <pranavpragyan.sk@gmail.com>
2023-03-18 02:41:23 +0530

    Third commit

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 function connectDatabase() {
-  mongoose.connect("mongodb+srv://Pranav:0gmajIxFhaoFXdTv@socialmedia.9ryvax1.mongodb.net/socialMedia").then(function (data) {
+  mongoose.connect(`mongodb+srv://Pranav:${process.env.PASSWORD}@socialmedia.9ryvax1.mongodb.net/socialMedia`).then(function (data) {
     console.log(`MongoDB is connect with host ${data.connection.host} `);
   }).catch(function (err) {
     console.log("MongoDB could not be connected");
